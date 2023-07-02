0c77170900f2aa0489d7c1ffe017ee06ca24b359
Wiliam Pereira Dias <80425917+WiliamDays@users.noreply.github.com>
2023-07-02 00:54:59 -0300

    Estilo do todo list

---
+++ script/script.js
@@ -0,0 +1,5 @@
+const form = document.getElementById('form')
+const  novatarefa = documetn.getElementById('novatarefa')
+const tarefaspFazer = document.getElementById('afazeres')
+
+const lista = JSON.parse(localStorage.getItem('afazeres')) //Criei uma variável JSON para guardar as tarefas inseridas no input
